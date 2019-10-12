e197bb0e49394fae555adacb88fa7cd41b157267
Debasish Ghosh <debasishghosh@Debasishs-MBP.fios-router.home>
2019-10-11 21:50:18 -0400

    Added my project 8TH

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var model = {
+  title: 'Azure Express',
+  message: process.env.MESSAGE || "This is development"
+ };
+
+  res.render('index', model);
 });
 
 module.exports = router;
