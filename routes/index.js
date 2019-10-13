0701efd343454532b21535c9000244a39ec3f454
Dev Dg <devg.dg85@outlook.com>
2019-10-12 21:36:57 -0400

    msg Y

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var model = {
     title: 'My favorite Azure',
-    mesage: process.env.MESSAGE || "This is development .. and you are watching a MAGIC"
+    mesage: process.env.MESSAGE || "Hi ... This is development .. and you are watching a MAGIC"
   };
   res.render('index', model);
 });
