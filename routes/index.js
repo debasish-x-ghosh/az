ce30e5bb5afa923fa8b14584ec2a2fbe48785ac3
Dev Dg <devg.dg85@outlook.com>
2019-10-13 09:32:04 -0400

    commit 5

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var model = {
-    title: 'My favorite Azure',
+    title: 'BR000000 ---> This is My favorite Azure',
     mesage: process.env.MESSAGE || "Hola ... hiiiiiiiiii  esta es una MAGIC ....."
   };
   res.render('index', model);
