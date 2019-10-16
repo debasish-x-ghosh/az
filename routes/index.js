a9090b4e817e1fa0192609dabdf0935b4d057329
Dev Dg <devg.dg85@outlook.com>
2019-10-15 20:53:06 -0400

    commit 1

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 var model = {
-  title: 'Azure Express',
-  message: process.env.MESSAGE || "This is development"
+  title: 'Fine Azure Express',
+  message: process.env.MESSAGE || "Super - This is development"
  };
 
   res.render('index', model);
